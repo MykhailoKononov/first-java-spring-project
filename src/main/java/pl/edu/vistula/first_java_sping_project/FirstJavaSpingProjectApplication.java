f188f1d436d161c507d07713d552818687addbc2
MykhailoKononov <kononomisha@gmail.com>
2026-01-06 17:36:00 +0100

    initial commit

---
+++ src/main/java/pl/edu/vistula/first_java_sping_project/FirstJavaSpingProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_java_sping_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJavaSpingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJavaSpingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/vistula/first_java_sping_project/FirstJavaSpingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_java_sping_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaSpingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
